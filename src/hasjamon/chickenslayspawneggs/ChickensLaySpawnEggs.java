96ec92d899a29f08abd4076a84ac7753abdce486
Bahm <x.benjamin.ahm@gmail.com>
2022-01-06 16:42:46 +0700

    Added /chickenbonus command.

--- src/hasjamon/chickenslayspawneggs/ChickensLaySpawnEggs.java
+++ src/hasjamon/chickenslayspawneggs/ChickensLaySpawnEggs.java
@@ -4,15 +4,21 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import oshi.util.tuples.Pair;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,7 +27,7 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.*;
 
-public class ChickensLaySpawnEggs extends JavaPlugin implements Listener {
+public class ChickensLaySpawnEggs extends JavaPlugin implements Listener, CommandExecutor {
     @Override
     public void onEnable() {
         // Do... something that's required to make config work properly
@@ -30,6 +36,9 @@ public class ChickensLaySpawnEggs extends JavaPlugin implements Listener {
         saveConfigAsDefault();
         // Register events
         getServer().getPluginManager().registerEvents(this, this);
+        // Register commands
+        PluginCommand chickenBonusCmd = this.getCommand("chickenbonus");
+        if(chickenBonusCmd != null) chickenBonusCmd.setExecutor(this);
     }
 
     @EventHandler
@@ -39,42 +48,50 @@ public class ChickensLaySpawnEggs extends JavaPlugin implements Listener {
 
         // If it's an egg and wasn't dropped by a player: Chance to lay a random spawn egg instead
         if (itemStack.getType() == Material.EGG && item.getThrower() == null && item.getPickupDelay() == 10) {
-            int radius = this.getConfig().getInt("named-chicken-radius");
-            List<Entity> nearbyEntities = item.getNearbyEntities(radius, radius, radius);
-            Set<String> namedChickensPos = new HashSet<>();
-            Map<Character, Integer> letterBonuses = new HashMap<>();
-
-            for(Entity ne : nearbyEntities){
-                if(ne.getType() == EntityType.CHICKEN){
-                    String chickenName = ne.getCustomName();
-
-                    if(chickenName != null) {
-                        Location loc = ne.getLocation();
-                        String pos = loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
-
-                        // If no other named chicken has been found at that location
-                        if (!namedChickensPos.contains(pos)) {
-                            namedChickensPos.add(pos);
-                            letterBonuses.merge(chickenName.toLowerCase().charAt(0), 1, Integer::sum);
-                        }
-                    }
-                }
-            }
+            Pair<Map<Character, Integer>, Integer> bonuses = calcChickenBonuses(item);
+            Map<Character, Integer> letterBonuses = bonuses.getA();
+            Integer numNamedChickens = bonuses.getB();
 
             double spawnChance = this.getConfig().getDouble("spawn-egg-chance");
-            if (Math.random() <= spawnChance * calcSpawnChanceBonus(namedChickensPos.size())) {
+            if (Math.random() <= spawnChance * calcGeneralChickenBonus(numNamedChickens)) {
                 itemStack.setType(getRandomSpawnEgg(letterBonuses));
             }
         }
     }
 
     // Returns log2(n + 2)
-    private double calcSpawnChanceBonus(double numNamedChickens){
+    public double calcGeneralChickenBonus(double numNamedChickens){
         // log2(x) = log(x) / log(2)
         return Math.log(numNamedChickens + 2) / Math.log(2);
     }
 
-    private Material getRandomSpawnEgg(Map<Character, Integer> letterBonuses){
+    public Pair<Map<Character, Integer>, Integer> calcChickenBonuses(Entity center) {
+        int radius = this.getConfig().getInt("named-chicken-radius");
+        List<Entity> nearbyEntities = center.getNearbyEntities(radius, radius, radius);
+        Set<String> namedChickensPos = new HashSet<>();
+        Map<Character, Integer> letterBonuses = new HashMap<>();
+
+        for(Entity ne : nearbyEntities){
+            if(ne.getType() == EntityType.CHICKEN){
+                String chickenName = ne.getCustomName();
+
+                if(chickenName != null) {
+                    Location loc = ne.getLocation();
+                    String pos = loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ();
+
+                    // If no other named chicken has been found at that location
+                    if (!namedChickensPos.contains(pos)) {
+                        namedChickensPos.add(pos);
+                        letterBonuses.merge(chickenName.toLowerCase().charAt(0), 1, Integer::sum);
+                    }
+                }
+            }
+        }
+
+        return new Pair<>(letterBonuses, namedChickensPos.size());
+    }
+
+    public Material getRandomSpawnEgg(Map<Character, Integer> letterBonuses){
         ConfigurationSection weightConfig = this.getConfig().getConfigurationSection("spawn-egg-weights");
         Random rand = new Random();
         int totalWeight = calcTotalWeight(letterBonuses);
@@ -135,5 +152,21 @@ public class ChickensLaySpawnEggs extends JavaPlugin implements Listener {
                 Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Failed to save default.yml");
             }
         }
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        if(sender instanceof Player player){
+            Pair<Map<Character, Integer>, Integer> bonuses = calcChickenBonuses(player);
+            Map<Character, Integer> letterBonuses = bonuses.getA();
+            Integer numNamedChickens = bonuses.getB();
+
+            double spawnChance = this.getConfig().getDouble("spawn-egg-chance");
+            double withBonus = spawnChance * calcGeneralChickenBonus(numNamedChickens);
+            player.sendMessage("§7Unique coords with named chickens nearby: " + numNamedChickens);
+            player.sendMessage("§7Chance to lay a spawn egg: " + Math.floor(withBonus * 10000) / 100 + "%");
+            player.sendMessage("§7Letter bonuses: " + letterBonuses.toString());
+            return true;
+        }
+        return false;
     }
 }
